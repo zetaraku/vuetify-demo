ebbc266ee826954fc2cc7671ae2e208acb2fb5a7
Raku Zeta <zetaraku@gmail.com>
2021-05-26 15:25:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# vite-vue2-template
+
+This is a minimal [Vite](https://vitejs.dev/) + [Vue 2](https://vuejs.org/) starter template, using [vite-plugin-vue2](https://github.com/underfin/vite-plugin-vue2).
+
+## Project setup
+```
+npm install
+```
+
+### Start development server
+```
+npm run dev
+```
+
+### Build for production
+```
+npm run build
+```
+
+### Locally preview production build
+```
+npm run dev
+```
+
+### Customize configuration
+See [Configuring Vite](https://vitejs.dev/config/) and [vite-plugin-vue2](https://github.com/underfin/vite-plugin-vue2).
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img
+      alt="Vue logo"
+      src="./assets/vue-logo.png"
+      style="width: 200px; padding: 0px"
+    />
+    <img
+      alt="Vite logo"
+      src="./assets/vite-logo.svg"
+      style="width: 200px; padding: 12px"
+    />
+    <HelloWorld msg="Hello Vue 2 + Vite" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
